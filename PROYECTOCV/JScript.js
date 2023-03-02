2eb6eda2ac1db73a93cebca82e1dd36bf4dd280e
AngyPuche <angypuche@gmail.com>
2023-03-01 22:23:15 -0300

    CV terminado

---
+++ PROYECTOCV/JScript.js
@@ -0,0 +1,11 @@
+
+document.getElementById('button').addEventListener('click', function(){
+    console.log("Hola Mundo desde EvenListener");
+    document.getElementById("datos").innerHTML = "Nombre Completo: Angélica María, Apellidos: Puche Orozco, Lugar de residencia: Luján, Provincia de Buenos Aires, Telefono: 11 31435579"
+});
+
+document.getElementById('button').addEventListener('click', function(){
+    console.log("Hola Mundo desde EvenListener");
+    document.getElementById("edad").innerHTML = "36 años"
+});
+
